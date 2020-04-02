01975d655973954c4c6f93337e395c891bbc3cc0
ToharFine <44654013+ToharFine@users.noreply.github.com>
2020-04-02 21:42:43 +0300

    2nd commit

--- PrimoAmbu.ino
+++ PrimoAmbu.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+/////////////////////////
+// Includes:
+#include <Servo.h>
+/////////////////////////
+//ports:
+int digital_limit_port = 0;
+int digital_encoder_port = 0;
+int belt_motor_port = 0;
+/////////////////////////
 
+
+void setup() {
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
+  
+ 
 }
