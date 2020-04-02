4d4803ae4a8349442a01d631015988ea366e8244
ToharFine <44654013+ToharFine@users.noreply.github.com>
2020-04-02 21:54:26 +0300

    tests

--- PrimoAmbu.ino
+++ PrimoAmbu.ino
@@ -7,10 +7,15 @@ int digital_limit_port = 0;
 int digital_encoder_port = 0;
 int belt_motor_port = 0;
 /////////////////////////
-
+//constants:
+/////////////////////////
+Servo beltMotor;
+int EncoderCounter = 0;
+#define getDigitalLimit() (digitalRead(digital_limit_port))
+/////////////////////////
 
 void setup() {
-  
+  pinMode(digital_limit_port, INPUT);
 }
 
 void loop() {
